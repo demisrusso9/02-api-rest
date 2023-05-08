11c3e0cf16275e389c9cbfa5d0e43b6adbdb9b49
Demis Russo <demisjunior@hotmail.com>
2023-05-08 15:58:32 -0300

    separating routes

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactionRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/', async (req, res) => {
-  const tables = await knex('transactions').select('*').returning('*')
-
-  return tables
-})
+app.register(transactionRoutes)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('Server running')
